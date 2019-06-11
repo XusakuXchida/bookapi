f02c25822a3b294a1f3e1e6105dcaf822d7daffb
YUSAKU UCHIDA <ni34ga@yahoo.co.jp>
2019-06-12 00:00:11 +0900

    fix json messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,29 +2,29 @@ class BooksController < ApplicationController
 
   def index
     books = Book.all
-    render json: { status: 'SUCCESS', message: 'Success GET books', data: books }    
+    render json: { message: 'SUCCESS GET books', data: books }    
   end
 
   def show
     book = Book.find(params[:id])
-    render json: { status: 'SUCCESS', message: 'Success GET book', data: book }
+    render json: { message: 'SUCCESS GET book', data: book }
   end
 
   def create
     book = Book.new(book_params)
     if book.save
-      render json: { status: 'SUCCESS', message: 'create book', data: book }
+      render json: { message: 'SUCCESS create book', data: book }
     else
-      render json: { status: 'ERROR', message: 'book cannot created', data: book.errors }
+      render json: { message: 'ERROR book cannot created' }
     end
   end
 
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-      render json: { status: 'SUCCESS', message: 'update book', data: book }
+      render json: { message: 'SUCCESS update book', data: book }
     else
-      render json: { status: 'ERROR', message: 'book cannot updated', data: book.errors }
+      render json: { message: 'ERROR book cannot updated' }
     end
   end
 
